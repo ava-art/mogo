152259adf532a45e04574e840c80096b17c65f6e
ava-art <koshkadav@gmail.com>
2022-03-25 14:37:35 +0300

    Create JQuery menu

---
+++ js/app.js
@@ -0,0 +1,96 @@
+$(function(){
+
+      var header = $("#header"),
+          introH = $("#intro").innerHeight(),
+          scrollOffset = $(window).scrollTop();
+
+
+          /*Fixed Scroll*/
+
+        chekScroll(scrollOffset);
+
+      $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+        chekScroll(scrollOffset);
+      });
+
+      function chekScroll(scrollOffset){
+        if( scrollOffset >= introH) {
+          header.addClass("fixed");
+        } else {
+          header.removeClass("fixed");
+        }
+      }
+
+
+      /*Smooth scroll*/
+      $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+          $("#nav a").removeClass("active");
+          $this.addClass("active")
+
+        $("html, body").animate({
+          scrollTop: blockOffset
+        },1000);
+      });
+
+
+      /* Menu nav toogle*/
+      $("#nav_toggle").on("click", function(event){
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+
+      });
+
+      $("#nav").on("click", function(event){
+        event.preventDefault();
+
+        $(this).removeClass("active");
+        $("#nav_toggle").removeClass("active");
+
+      });
+
+      $("#header_logo").on("click", function(event){
+        event.preventDefault();
+
+        $(this).removeClass("active");
+        $("#nav_toggle").removeClass("active");
+      });
+
+
+      /*Collapse*/
+
+      $("[data-collapse]").on("click", function(event){
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $(this).data('collapse');
+
+            $(this).toggleClass("active");
+
+      });
+
+
+      /*Slider*/
+
+      $("[data-slider]").slick({
+        infinite: true,
+        fade:false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+
+
+
+
+
+
+
+});
